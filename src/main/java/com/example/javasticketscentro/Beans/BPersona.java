b6047efa071eef67d89fba5a737cf0a847c9b1cb
Stefhanie Jaramillo <a20190674@pucp.edu.pe>
2022-06-05 17:51:28 -0500

    Initial commit

---
+++ src/main/java/com/example/javasticketscentro/Beans/BPersona.java
@@ -0,0 +1,126 @@
+package com.example.javasticketscentro.Beans;
+
+import java.util.Date;
+
+public class BPersona {
+    private int idPer;
+    private String nombre;
+    private int dni;
+    private String apellido;
+    private int numCel;
+    private int foto;
+    private Date fecha_Nc;
+    private String email;
+    private String usuario;
+    private String contrasenia;
+    private String direccion;
+    private String rol;
+    private int codigoPUCP;
+
+    public int getIdPer() {
+        return idPer;
+    }
+
+    public void setIdPer(int idPer) {
+        this.idPer = idPer;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumCel() {
+        return numCel;
+    }
+
+    public void setNumCel(int numCel) {
+        this.numCel = numCel;
+    }
+
+    public int getFoto() {
+        return foto;
+    }
+
+    public void setFoto(int foto) {
+        this.foto = foto;
+    }
+
+    public Date getFecha_Nc() {
+        return fecha_Nc;
+    }
+
+    public void setFecha_Nc(Date fecha_Nc) {
+        this.fecha_Nc = fecha_Nc;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+
+    public int getCodigoPUCP() {
+        return codigoPUCP;
+    }
+
+    public void setCodigoPUCP(int codigoPUCP) {
+        this.codigoPUCP = codigoPUCP;
+    }
+
+
+
+}
---
+++ src/main/java/com/example/javasticketscentro/Daos/AdminDao.java
@@ -0,0 +1,44 @@
+package com.example.javasticketscentro.Daos;
+import com.example.javasticketscentro.Beans.BPersona;
+import java.sql.*;
+import java.util.ArrayList;
+
+public class AdminDao {
+    public static ArrayList<BPersona> listarCliente() {
+        ArrayList<BPersona> listaClientes = new ArrayList<>();
+        String user = "root";
+        String pass = "root";
+        String url = "jdbc:mysql://localhost:3306/centro1";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+
+        try (Connection connection = DriverManager.getConnection(url, user, pass);
+             Statement stmt = connection.createStatement();
+             ResultSet rs = stmt.executeQuery("select idPersona,nombre,apellido,numeroCelular,fechaDeNacimiento,email,direccionCliente from persona");) {
+
+            while (rs.next()) {
+                BPersona bPersona = new BPersona();
+
+                bPersona.setIdPer(rs.getInt(1));
+                bPersona.setNombre(rs.getString(2));
+                bPersona.setApellido(rs.getString(3));
+                bPersona.setNumCel(rs.getInt(4));
+                bPersona.setFecha_Nc(rs.getDate(5));
+                bPersona.setEmail(rs.getString(6));
+                bPersona.setDireccion(rs.getString(7));
+                listaClientes.add(bPersona);
+
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return listaClientes;
+    }
+}
