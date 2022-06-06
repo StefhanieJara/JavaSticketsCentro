89b3f06264e32e8909dc746e042ccb427695451d
raykevinconcha <tudela2352>
2022-06-05 23:54:20 -0500

    servlet

---
+++ src/main/java/com/example/javasticketscentro/Servlets/UsuarioHistorial_2Servlet.java
@@ -0,0 +1,20 @@
+package com.example.javasticketscentro.Servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "UsuarioHistorial_2Servlet", value = "/UsuarioHistorial_2Servlet")
+public class UsuarioHistorial_2Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("Cliente/UsuarioHistorial_2.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
---
+++ src/main/java/com/example/javasticketscentro/Servlets/Usuarioindex_2Servlet.java
@@ -0,0 +1,20 @@
+package com.example.javasticketscentro.Servlets;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "Usuarioindex_2Servlet", value = "/Usuarioindex_2Servlet")
+public class Usuarioindex_2Servlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("Cliente/Usuarioindex_2Servlet.jsp");
+        requestDispatcher.forward(request,response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
