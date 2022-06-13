43448aaa0a1312fa5d5cd6629ae7dbaabc5edfc1
joelsilvera <joel.silvera@pucp.edu.pe>
2022-06-13 01:16:36 -0500

    Querys Historial

--- src/main/java/com/example/javasticketscentro/Daos/HistorialDao.java
+++ src/main/java/com/example/javasticketscentro/Daos/HistorialDao.java
@@ -21,7 +21,27 @@ public class HistorialDao {
             throw new RuntimeException(e);
         }
 
-        String sql = "select * from centro1 where blabla";
+        String sql = "SELECT distinct subquery.FechaDeCompra as 'Fecha de compra', " +
+                " subquery.Codigo as 'Codigo', " +
+                " subquery.Total as 'Total' " +
+                "FROM (SELECT p.idPersona as Usuario, " +
+                "   c.fechaCompra as FechaDeCompra, " +
+                "   c.idCompra as Codigo, " +
+                "   se.nombre as Sede, " +
+                "        concat(f.fecha,' - ',f.horaInicio) as FechaDeLaFuncion, " +
+                "        c.montoTotal as Total, " +
+                "        t.cantidadButaca as Unidades, " +
+                "        pel.nombre as Pelicula, " +
+                "        f.precio as PrecioPorTicket " +
+                "FROM persona p " +
+                "    inner join compra c on (p.idPersona = c.Persona_idPersona)" +
+                "    inner join ticket t on (c.idCompra = t.Compra_idCompra) " +
+                "    inner join funcion f on (t.Funcion_idFuncion = f.idFuncion) " +
+                "    inner join pelicula pel on (f.Pelicula_idPelicula = pel.idPelicula) " +
+                "    inner join funcion_has_sala fs on (f.idFuncion = fs.Funcion_idFuncion) " +
+                "    inner join sala sa on (fs.Sala_idSala = sa.idSala) " +
+                "    inner join sede se on (sa.Sede_idSede = se.idSede) " +
+                "WHERE p.idPersona = 1) AS subquery;";
 
         try (Connection connection = DriverManager.getConnection(url, user, pass);
              PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
@@ -59,7 +79,30 @@ public class HistorialDao {
             throw new RuntimeException(e);
         }
 
-        String sql = "select * from centro1 where blabla";
+        String sql = "SELECT tablita.Unidades as 'Unidades', " +
+                " tablita.Pelicula as 'Pelicula', " +
+                "        tablita.Sede as 'Sede', " +
+                "        tablita.FechaDeLaFuncion as 'Fecha de la funcion', " +
+                "        tablita.PrecioPorTicket as 'Precio por ticket' " +
+                "FROM (SELECT p.idPersona as Usuario, " +
+                "   c.fechaCompra as FechaDeCompra, " +
+                "   c.idCompra as Codigo, " +
+                "   se.nombre as Sede, " +
+                "   concat(f.fecha,' - ',f.horaInicio) as FechaDeLaFuncion, " +
+                "   c.montoTotal as Total, " +
+                "   t.cantidadButaca as Unidades, " +
+                "   pel.nombre as Pelicula, " +
+                "   f.precio as PrecioPorTicket " +
+                "FROM persona p " +
+                "    inner join compra c on (p.idPersona = c.Persona_idPersona) " +
+                "    inner join ticket t on (c.idCompra = t.Compra_idCompra) " +
+                "    inner join funcion f on (t.Funcion_idFuncion = f.idFuncion) " +
+                "    inner join pelicula pel on (f.Pelicula_idPelicula = pel.idPelicula) " +
+                "    inner join funcion_has_sala fs on (f.idFuncion = fs.Funcion_idFuncion) " +
+                "    inner join sala sa on (fs.Sala_idSala = sa.idSala) " +
+                "    inner join sede se on (sa.Sede_idSede = se.idSede) " +
+                "WHERE p.idPersona = 1) AS tablita " +
+                "WHERE tablita.Codigo = '10793DD';";
 
         try (Connection connection = DriverManager.getConnection(url, user, pass);
              PreparedStatement preparedStatement = connection.prepareStatement(sql);) {
