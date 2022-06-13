66c0f2d15dd79e07838fb137e8459fca6c1c3148
Jun23333333 <106772376+Jun23333333@users.noreply.github.com>
2022-06-12 20:12:36 -0500

    Update Bhistorial.java

--- src/main/java/com/example/javasticketscentro/Beans/Bhistorial.java
+++ src/main/java/com/example/javasticketscentro/Beans/Bhistorial.java
@@ -4,13 +4,7 @@ public class Bhistorial {
     private String fecha_compra;
     private String codigo;
     private String sede;
-    private String fecha_funcion;
     private double total;
-    private String estado;
-    private int unidadades;
-    private String pelicula;
-    private double precio_por_peli;
-    private double total_detalle;
 
     public String getFecha_compra() {
         return fecha_compra;
@@ -36,59 +30,11 @@ public class Bhistorial {
         this.sede = sede;
     }
 
-    public String getFecha_funcion() {
-        return fecha_funcion;
-    }
-
-    public void setFecha_funcion(String fecha_funcion) {
-        this.fecha_funcion = fecha_funcion;
-    }
-
     public double getTotal() {
         return total;
     }
 
     public void setTotal(double total) {
         this.total = total;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public int getUnidadades() {
-        return unidadades;
-    }
-
-    public void setUnidadades(int unidadades) {
-        this.unidadades = unidadades;
-    }
-
-    public String getPelicula() {
-        return pelicula;
-    }
-
-    public void setPelicula(String pelicula) {
-        this.pelicula = pelicula;
-    }
-
-    public double getPrecio_por_peli() {
-        return precio_por_peli;
-    }
-
-    public void setPrecio_por_peli(double precio_por_peli) {
-        this.precio_por_peli = precio_por_peli;
-    }
-
-    public double getTotal_detalle() {
-        return total_detalle;
-    }
-
-    public void setTotal_detalle(double total_detalle) {
-        this.total_detalle = total_detalle;
     }
 }
