fbd3cf034e4f34dfb53dda5524dc077a9be286e8
Jun23333333 <106772376+Jun23333333@users.noreply.github.com>
2022-06-12 20:12:52 -0500

    Add files via upload

---
+++ src/main/java/com/example/javasticketscentro/Beans/Bhistorial_detalle.java
@@ -0,0 +1,49 @@
+package com.example.javasticketscentro.Beans;
+
+public class Bhistorial_detalle {
+    private String unidades;
+    private String pelicula;
+    private String fecha;
+    private String precio;
+    private String estado;
+
+    public String getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(String unidades) {
+        this.unidades = unidades;
+    }
+
+    public String getPelicula() {
+        return pelicula;
+    }
+
+    public void setPelicula(String pelicula) {
+        this.pelicula = pelicula;
+    }
+
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(String precio) {
+        this.precio = precio;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
